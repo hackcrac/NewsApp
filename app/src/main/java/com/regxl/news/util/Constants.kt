afee721006858b98d2460ff4442771a07e2fbecd
hackcrac <33978646+hackcrac@users.noreply.github.com>
2022-08-12 18:26:58 +0700

    Update Constants.kt

--- app/src/main/java/com/regxl/news/util/Constants.kt
+++ app/src/main/java/com/regxl/news/util/Constants.kt
@@ -3,6 +3,6 @@ package com.regxl.news.util
 class Constants {
     companion object{
         const val BASE_URL = "https://newsapi.org/"
-        const val API_KEY = "cefa87a5dd07420a9c861423ed9cbec4"
+        const val API_KEY = "Enter Your Own API KEY"
     }
 }
